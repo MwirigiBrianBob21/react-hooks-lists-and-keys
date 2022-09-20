e307179428ee809b9800524fc9a184caffb63a86
MwirigiBrianBob21 <brian.mwirigi@students.moringaschool.com>
2022-09-20 20:16:49 +0300

    Complete assignment

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ColorItem from "./ColorItem"
 
 function ColorList() {
   const colors = [
@@ -9,15 +10,29 @@ function ColorList() {
     "hotpink",
   ];
 
+  // Using array.map method to change the strings to array of JSX elements
+
+  const colorElements = colors.map((color) => {
+    return <ColorItem key={color} color={color} />
+    // return <li key ={color} style={{color: color}}>{color}</li>
+  });
+////// Very important
+  // the key prop allows React internally to keep track of each element in the array of JSX
+
+  // Anytime you are creating an array of JSX elements, you must use the key prop
+
   return (
     <div>
       <h1>Top 5 CSS Colors</h1>
       <ol>
-        <li style={{ color: colors[0] }}>{colors[0]}</li>
+
+        {colorElements}
+        {/* The code below was before we used .map method */}
+        {/* <li style={{ color: colors[0] }}>{colors[0]}</li>
         <li style={{ color: colors[1] }}>{colors[1]}</li>
         <li style={{ color: colors[2] }}>{colors[2]}</li>
         <li style={{ color: colors[3] }}>{colors[3]}</li>
-        <li style={{ color: colors[4] }}>{colors[4]}</li>
+        <li style={{ color: colors[4] }}>{colors[4]}</li> */}
       </ol>
     </div>
   );
